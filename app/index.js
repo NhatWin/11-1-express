72e9ba6d8c7532a257c84286aed3e87ab46dd1f2
NhatWin <nhat0933@gmail.com>
2022-12-15 20:21:59 -0600

    dynamic param

--- app/index.js
+++ app/index.js
@@ -1,1 +1,28 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import express from "express";
+import termsData from "./db/terms.json" assert { type: "json" };
+
+// Start express
+const app = express();
+const port = 3001;
+
+app.get("/api/terms", (_, res) => {
+  res.json(termsData);
+});
+
+app.get("/api/terms/:term", (req, res) => {
+  const { term } = req.params;
+
+  const requestedTerm = termsData.find(
+    (t) => t.term.toUpperCase() === term.toUpperCase()
+  );
+
+  if (requestedTerm) {
+    res.json(requestedTerm);
+  } else {
+    res.status(404).json({ error: `Term ${term} not found. :(` });
+  }
+});
+
+app.listen(port, () => {
+  console.info("server running");
+});
